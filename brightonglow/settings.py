27c1a085fb0c4e4fde36d3a18c5c8c0bf4992ddf
Brogan <brogancarpenter@hotmail.co.uk>
2025-02-12 17:40:21 +0000

    Added all products to the database using django shell bulk upload, create category model and test

--- brightonglow/settings.py
+++ brightonglow/settings.py
@@ -10,10 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files (CSS, JS, images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Global static folder
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Media files (User-uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
--- products/models.py
+++ products/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
