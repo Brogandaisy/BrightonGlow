d87f1748b4a01a339ffb5a094b94370151f24d66
Brogan <brogancarpenter@hotmail.co.uk>
2025-02-22 17:13:10 +0000

    bag_detail.html changes to allow product quantity change and remove product change to delete. adjust bag views.py and include bag_adjust to urls.py

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.bag_detail, name='bag_detail'),
     path('add/<int:product_id>/', views.bag_add, name='bag_add'),
+    path('adjust/<int:product_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<int:product_id>/', views.bag_remove, name='bag_remove'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 # bag/views.py (Add Logging to Find Decimal Issue)
 import json
 import logging
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
 from .bag import Bag
@@ -21,11 +22,14 @@ def bag_add(request, product_id):
 
     return redirect('bag_detail')
 
+@require_POST
 def bag_remove(request, product_id):
     bag = Bag(request)
     product = get_object_or_404(Product, id=product_id)
     bag.remove(product)
+    request.session['total'] = bag.get_total_price()
     return redirect('bag_detail')
+
 
 def bag_detail(request):
     bag = Bag(request)
@@ -40,3 +44,17 @@ def bag_detail(request):
     request.session.modified = True
 
     return render(request, 'bag/bag_detail.html', {'bag': bag, 'total': total})
+
+@require_POST
+def adjust_bag(request, product_id):
+    bag = Bag(request)
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get('quantity'))
+
+    if quantity > 0:
+        bag.add(product=product, quantity=quantity, update_quantity=True)
+    else:
+        bag.remove(product)
+
+    request.session['total'] = bag.get_total_price()
+    return redirect('bag_detail')
