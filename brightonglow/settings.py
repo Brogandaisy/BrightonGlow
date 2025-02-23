cf9601b4f4c62e65e07be5df1cb7af19d3f00ac7
Brogan <brogancarpenter@hotmail.co.uk>
2025-02-23 22:06:52 +0000

    Add in core styling, fonts, buttons and colours

--- brightonglow/settings.py
+++ brightonglow/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JS, images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # Global static folder
+    os.path.join(BASE_DIR, "static"),  
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
