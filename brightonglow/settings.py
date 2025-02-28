8c91af9e995d61aec8d3f3d4b2f1b64ca97150c0
Brogan <brogancarpenter@hotmail.co.uk>
2025-02-28 23:34:22 +0000

    Update email settings

--- brightonglow/settings.py
+++ brightonglow/settings.py
@@ -174,6 +174,19 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if os.getenv('DEVELOPMENT') == 'True':
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'brightonglowskincare@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS') 
+    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_EMAIL')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
