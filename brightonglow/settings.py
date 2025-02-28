2a05b1b25f507664d86594bfedc95565245f4d60
Brogan <brogancarpenter@hotmail.co.uk>
2025-02-28 18:47:15 +0000

    Fixed static files directory

--- brightonglow/settings.py
+++ brightonglow/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # Static files (CSS, JS, images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
