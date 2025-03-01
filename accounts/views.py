83d452de339b31c81d8dea0075764ed73e30ff39
Brogan <brogancarpenter@hotmail.co.uk>
2025-03-01 20:15:01 +0000

    Add email for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from .models import Customer
 from django.contrib.auth.decorators import login_required
 from .forms import CustomerProfileForm
 from orders.models import Order
-
+from django.core.mail import send_mail
 
 
 def register(request):
@@ -13,24 +13,23 @@ def register(request):
         form = CustomUserCreationForm(request.POST)  
         if form.is_valid():
             user = form.save()
-            Customer.objects.create(user=user, email=form.cleaned_data.get('email'))  # Save email to Customer
+            email = form.cleaned_data.get('email')
+            Customer.objects.create(user=user, email=email)
+
             login(request, user)
+
+            subject = "Welcome to Brighton GLOW!"
+            message = f"Hi {user.username},\n\nThank you for registering at Brighton GLOW. We are excited to have you!"
+            sender = settings.DEFAULT_FROM_EMAIL
+            recipient_list = [email]
+
+            send_mail(subject, message, sender, recipient_list, fail_silently=False)
+
             return redirect('home')
     else:
         form = CustomUserCreationForm()
+    
     return render(request, 'accounts/register.html', {'form': form})
-
-@login_required
-def update_profile(request):
-    customer = request.user.customer  
-    if request.method == 'POST':
-        form = CustomerProfileForm(request.POST, instance=customer)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')  
-    else:
-        form = CustomerProfileForm(instance=customer) 
-    return render(request, 'accounts/update_profile.html', {'form': form})
 
 @login_required
 def profile(request):
