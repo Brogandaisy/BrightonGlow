024ae8adfa666d36584700fa3e06fec13c7b19e4
Brogan <brogancarpenter@hotmail.co.uk>
2025-05-13 21:41:47 +0100

    Add about.html using custom model, replace existing about.html, and update styling

--- about/models.py
+++ about/models.py
@@ -5,6 +5,7 @@ class AboutPage(models.Model):
     header = models.CharField(max_length=200)
     sub_heading = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField()
+    image = models.ImageField(upload_to='about/', blank=True, null=True)
 
     def __str__(self):
         return self.header
