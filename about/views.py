f10cb3d71f76970043dec165cf60790cf00a65c7
Brogan <brogancarpenter@hotmail.co.uk>
2025-05-13 23:15:42 +0100

    remove featured products debug

--- about/views.py
+++ about/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from .models import AboutPage
-from products.models import Product
 
 
 def about_view(request):
     about = AboutPage.objects.first()
-    featured_products = Product.objects.order_by('?')[:3]
     return render(request, 'about/about.html', {
         'about': about,
-        'featured_products': featured_products,
     })
