f5b1f865627f31599e5140f203c1a8da97f7b438
Brogan <brogancarpenter@hotmail.co.uk>
2025-05-13 23:30:43 +0100

    Remove image in models.py about

--- about/models.py
+++ about/models.py
@@ -5,7 +5,6 @@ class AboutPage(models.Model):
     header = models.CharField(max_length=200)
     sub_heading = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField()
-    image = models.ImageField(upload_to='about/', blank=True, null=True)
 
     def __str__(self):
         return self.header
